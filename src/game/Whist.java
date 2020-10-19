d5e2d4b18c0913054f6a3948c6aa8c4b578d2d9b
4321louis <45321dumkof@gmail.com>
2020-10-19 21:31:55 +1100

    Update Whist.java

--- src/game/Whist.java
+++ src/game/Whist.java
@@ -11,85 +11,84 @@ import java.util.concurrent.ThreadLocalRandom;
 @SuppressWarnings("serial")
 public class Whist extends CardGame {
 	
-  public enum Suit
-  {
-    SPADES, HEARTS, DIAMONDS, CLUBS
-  }
+	public enum Suit
+	{
+    	SPADES, HEARTS, DIAMONDS, CLUBS
+	}
 
-  public enum Rank
-  {
+	public enum Rank
+	{
     // Reverse order of rank importance (see rankGreater() below)
 	// Order of cards is tied to card images
 	ACE, KING, QUEEN, JACK, TEN, NINE, EIGHT, SEVEN, SIX, FIVE, FOUR, THREE, TWO
-  }
-  
-  final String trumpImage[] = {"bigspade.gif","bigheart.gif","bigdiamond.gif","bigclub.gif"};
+	}
 
-  static final Random random = ThreadLocalRandom.current();
-  
+	final String trumpImage[] = {"bigspade.gif","bigheart.gif","bigdiamond.gif","bigclub.gif"};
+
+	static final Random random = ThreadLocalRandom.current();
+
   // return random Enum value
-  public static <T extends Enum<?>> T randomEnum(Class<T> clazz){
+	public static <T extends Enum<?>> T randomEnum(Class<T> clazz){
       int x = random.nextInt(clazz.getEnumConstants().length);
       return clazz.getEnumConstants()[x];
   }
-  
+
   // return random Card from Hand
-  public static Card randomCard(Hand hand){
-      int x = random.nextInt(hand.getNumberOfCards());
-      return hand.get(x);
-  }
- 
+	public static Card randomCard(Hand hand){
+		int x = random.nextInt(hand.getNumberOfCards());
+		return hand.get(x);
+	}
   // return random Card from ArrayList
-  public static Card randomCard(ArrayList<Card> list){
+	public static Card randomCard(ArrayList<Card> list){
       int x = random.nextInt(list.size());
       return list.get(x);
   }
-  
-  public boolean rankGreater(Card card1, Card card2) {
+	
+	public boolean rankGreater(Card card1, Card card2) {
 	  return card1.getRankId() < card2.getRankId(); // Warning: Reverse rank order of cards (see comment on enum)
   }
-	 
-  private final String version = "1.0";
-  public final int nbPlayers = 4;
-  public final int nbStartCards = 13;
-  public final int winningScore = 24;
-  private final int handWidth = 400;
-  private final int trickWidth = 40;
-  private final Deck deck = new Deck(Suit.values(), Rank.values(), "cover");
-  private final Location[] handLocations = {
-			  new Location(350, 625),
-			  new Location(75, 350),
-			  new Location(350, 75),
-			  new Location(625, 350)
-	  };
-  private final Location[] scoreLocations = {
-			  new Location(575, 675),
-			  new Location(25, 575),
-			  new Location(575, 25),
-			  new Location(650, 575)
-	  };
-  private Actor[] scoreActors = {null, null, null, null };
-  private final Location trickLocation = new Location(350, 350);
-  private final Location textLocation = new Location(350, 450);
-  private final int thinkingTime = 2000;
-  private Hand[] hands;
-  private Location hideLocation = new Location(-500, - 500);
-  private Location trumpsActorLocation = new Location(50, 50);
-  private boolean enforceRules=false;
 
-  public void setStatus(String string) { setStatusText(string); }
-  
+	private final String version = "1.0";
+	public final int nbPlayers = 4;
+	public final int nbStartCards = 13;
+	public final int winningScore = 24;
+	private final int handWidth = 400;
+	private final int trickWidth = 40;
+	private final Deck deck = new Deck(Suit.values(), Rank.values(), "cover");
+	private final Location[] handLocations = {
+		new Location(350, 625),
+		new Location(75, 350),
+		new Location(350, 75),
+		new Location(625, 350)
+	};
+	private final Location[] scoreLocations = {
+		new Location(575, 675),
+		new Location(25, 575),
+		new Location(575, 25),
+		new Location(650, 575)
+	};
+	private Actor[] scoreActors = {null, null, null, null };
+	private final Location trickLocation = new Location(350, 350);
+	private final Location textLocation = new Location(350, 450);
+	private final int thinkingTime = 2000;
+	private Hand[] hands;
+	private Location hideLocation = new Location(-500, - 500);
+	private Location trumpsActorLocation = new Location(50, 50);
+	private boolean enforceRules=false;
+
+	public void setStatus(String string) { setStatusText(string); }
+
 private int[] scores = new int[nbPlayers];
 
 Font bigFont = new Font("Serif", Font.BOLD, 36);
 
 private void initScore() {
-	 for (int i = 0; i < nbPlayers; i++) {
-		 scores[i] = 0;
-		 scoreActors[i] = new TextActor("0", Color.WHITE, bgColor, bigFont);
-		 addActor(scoreActors[i], scoreLocations[i]);
-	 }
-  }
+	for (int i = 0; i < nbPlayers; i++) {
+		scores[i] = 0;
+		scoreActors[i] = new TextActor("0", Color.WHITE, bgColor, bigFont);
+		addActor(scoreActors[i], scoreLocations[i]);
+	}
+}
 
 private void updateScore(int player) {
 	removeActor(scoreActors[player]);
@@ -100,31 +99,32 @@ private void updateScore(int player) {
 private Card selected;
 
 private void initRound() {
-		 hands = deck.dealingOut(nbPlayers, nbStartCards); // Last element of hands is leftover cards; these are ignored
-		 for (int i = 0; i < nbPlayers; i++) {
-			   hands[i].sort(Hand.SortType.SUITPRIORITY, true);
-		 }
-		 // Set up human player for interaction
-		CardListener cardListener = new CardAdapter()  // Human Player plays card
-			    {
-			      public void leftDoubleClicked(Card card) { selected = card; hands[0].setTouchEnabled(false); }
-			    };
-		hands[0].addCardListener(cardListener);
-		 // graphics
-	    RowLayout[] layouts = new RowLayout[nbPlayers];
-	    for (int i = 0; i < nbPlayers; i++) {
-	      layouts[i] = new RowLayout(handLocations[i], handWidth);
-	      layouts[i].setRotationAngle(90 * i);
-	      // layouts[i].setStepDelay(10);
-	      hands[i].setView(this, layouts[i]);
-	      hands[i].setTargetArea(new TargetArea(trickLocation));
-	      hands[i].draw();
-	    }
-	    
+	hands = deck.dealingOut(nbPlayers, nbStartCards); // Last element of hands is leftover cards; these are ignored
+	for (int i = 0; i < nbPlayers; i++) {
+		hands[i].sort(Hand.SortType.SUITPRIORITY, true);
+	}
+	// Set up human player for interaction
+	CardListener cardListener = new CardAdapter()  // Human Player plays card
+		{
+			@Override
+			public void leftDoubleClicked(Card card) { selected = card; hands[0].setTouchEnabled(false); }
+		};
+	hands[0].addCardListener(cardListener);
+	// graphics
+	RowLayout[] layouts = new RowLayout[nbPlayers];
+	for (int i = 0; i < nbPlayers; i++) {
+		layouts[i] = new RowLayout(handLocations[i], handWidth);
+		layouts[i].setRotationAngle(90 * i);
+		// layouts[i].setStepDelay(10);
+		hands[i].setView(this, layouts[i]);
+		hands[i].setTargetArea(new TargetArea(trickLocation));
+		hands[i].draw();
+	}
+
 //	    for (int i = 1; i < nbPlayers; i++)  // This code can be used to visually hide the cards in a hand (make them face down)
 //	      hands[i].setVerso(true);
 	    // End graphics
- }
+}
 
 private String printHand(ArrayList<Card> cards) {
 	String out = "";
@@ -189,28 +189,28 @@ private Optional<Integer> playRound() {  // Returns winner, if any
 				// Check: Following card must follow suit if possible
 					if (selected.getSuit() != lead && hands[nextPlayer].getNumberOfCardsWithSuit(lead) > 0) {
 						 // Rule violation
-						 String violation = "Follow rule broken by player " + nextPlayer + " attempting to play " + selected;
+						String violation = "Follow rule broken by player " + nextPlayer + " attempting to play " + selected;
 						 //System.out.println(violation);
-						 if (enforceRules) 
-							 try {
-								 throw(new BrokeRuleException(violation));
+						if (enforceRules) 
+							try {
+								throw(new BrokeRuleException(violation));
 								} catch (BrokeRuleException e) {
 									e.printStackTrace();
 									System.out.println("A cheating player spoiled the game!");
 									System.exit(0);
 								}  
-					 }
+					}
 				// End Check
 				 selected.transfer(trick, true); // transfer to trick (includes graphic effect)
-				 System.out.println("Winning card: "+winningCard.toString());
-				 System.out.println("Player "+nextPlayer+" play: "+selected.toString()+" from ["+printHand(hands[nextPlayer].getCardList())+"]");
+				System.out.println("Winning card: "+winningCard.toString());
+				System.out.println("Player "+nextPlayer+" play: "+selected.toString()+" from ["+printHand(hands[nextPlayer].getCardList())+"]");
 				 if ( // beat current winner with higher card
-					 (selected.getSuit() == winningCard.getSuit() && rankGreater(selected, winningCard)) ||
+					(selected.getSuit() == winningCard.getSuit() && rankGreater(selected, winningCard)) ||
 					  // trumped when non-trump was winning
-					 (selected.getSuit() == trumps && winningCard.getSuit() != trumps)) {
-					 winner = nextPlayer;
-					 winningCard = selected;
-				 }
+					(selected.getSuit() == trumps && winningCard.getSuit() != trumps)) {
+					winner = nextPlayer;
+					winningCard = selected;
+				}
 			// End Follow
 		}
 		delay(600);
@@ -227,7 +227,7 @@ private Optional<Integer> playRound() {  // Returns winner, if any
 	return Optional.empty();
 }
 
-  public Whist()
+	public Whist()
   {
     super(700, 700, 30);
     setTitle("Whist (V" + version + ") Constructed for UofM SWEN30006 with JGameGrid (www.aplu.ch)");
@@ -243,9 +243,9 @@ private Optional<Integer> playRound() {  // Returns winner, if any
     refresh();
   }
 
-  public static void main(String[] args)
-  {
+	public static void main(String[] args)
+	{
     new Whist();
-  }
+	}
 
 }
